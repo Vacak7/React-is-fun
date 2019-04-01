8901182ff846389148cbf8f740ead7bccfb6c362
Laszlo Borbiro <lborbiro@lanetelecom.com>
2019-04-01 16:37:48 +0100

    React-is-awsome

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
+var style = {
+    backgroundColor: 'orange',
+    color: 'white',
+    fontFamily: 'Arial'
+}
+
 const title = React.createElement(
    'h1',
-   {id: 'title', className: 'header'},
-   'Hello World'
+   {id: 'title', className: 'header', style: style},
+   'React App is Awesome'
 
 )
 
